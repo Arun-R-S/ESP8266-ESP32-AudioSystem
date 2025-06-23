fb16a6966cf9b947b0d93a7ee8c5b05ffb4829c1
Arun-R-S <arunrs0966@gmail.com>
2025-06-23 19:29:01 +0530

    update

--- src/drivers/ButtonHandler.cpp
+++ src/drivers/ButtonHandler.cpp
@@ -1,52 +1,38 @@
 #include "ButtonHandler.h"
 
-// Static instances
 AudioIC* ButtonHandler::_audioIC = nullptr;
 LCDDisplay* ButtonHandler::_lcd = nullptr;
 
 ButtonHandler::ButtonHandler()
-  : _btn1(&_config, BUTTON1_PIN), _btn2(&_config, BUTTON2_PIN) {
-}
+  : _btn1(&_config, BUTTON1_PIN), _btn2(&_config, BUTTON2_PIN) {}
 
 void ButtonHandler::begin() {
   Logger::info("Initializing ButtonHandler");
-
   pinMode(BUTTON1_PIN, INPUT_PULLUP);
   pinMode(BUTTON2_PIN, INPUT_PULLUP);
-
   _config.setEventHandler(handleEvent);
-
-  Logger::info("ButtonHandler initialized on pins %d, %d", BUTTON1_PIN, BUTTON2_PIN);
 }
 
 void ButtonHandler::update(AudioIC& audioIC, LCDDisplay& lcd) {
   _audioIC = &audioIC;
   _lcd = &lcd;
-
-  try {
-    _btn1.check();
-    _btn2.check();
-  } catch (...) {
-    Logger::error("Exception in ButtonHandler::update()");
-  }
+  _btn1.check();
+  _btn2.check();
 }
 
-void ButtonHandler::handleEvent(AceButton* button, uint8_t eventType, uint8_t buttonState) {
+void ButtonHandler::handleEvent(AceButton* button, uint8_t eventType, uint8_t state) {
   uint8_t pin = button->getPin();
 
-  try {
-    Logger::debug("Button event on pin %d: type=%d, state=%d", pin, eventType, buttonState);
-
-    if (eventType == AceButton::kEventReleased) {
-      if (pin == BUTTON1_PIN) {
-        _audioIC->adjustVolume(+1); // Unified method
-        _lcd->printLine("Volume Up", 1);
-      } else if (pin == BUTTON2_PIN) {
-        _audioIC->adjustVolume(-1);
-        _lcd->printLine("Volume Down", 1);
-      }
+  Logger::debug("Button event: pin=%d type=%d", pin, eventType);
+  if (eventType == AceButton::kEventReleased) {
+    if (pin == BUTTON1_PIN) {
+      int vol = _audioIC->getVolume() + 1;
+      _audioIC->setVolume(constrain(vol, 0, 48));
+      _lcd->printLine(1, "Volume Up");
+    } else if (pin == BUTTON2_PIN) {
+      int vol = _audioIC->getVolume() - 1;
+      _audioIC->setVolume(constrain(vol, 0, 48));
+      _lcd->printLine(1, "Volume Down");
     }
-  } catch (...) {
-    Logger::error("Exception in ButtonHandler::handleEvent()");
   }
 }
