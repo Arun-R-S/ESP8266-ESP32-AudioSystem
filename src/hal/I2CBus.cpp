58c203533122a499ff05c3ea4d5b480ae252791a
Arun-R-S <arunrs0966@gmail.com>
2025-06-27 21:46:50 +0530

    I2C Scan

--- src/hal/I2CBus.cpp
+++ src/hal/I2CBus.cpp
@@ -26,3 +26,24 @@ bool I2CBus::Write(uint8_t address, const uint8_t* data, uint8_t length) {
     Wire.write(data, length);
     return Wire.endTransmission() == 0;
 }
+
+void I2CBus::Scan() {
+    AddLogInfo("I2CBus", "Scanning for I2C devices...");
+
+    uint8_t count = 0;
+    for (uint8_t address = 1; address < 127; address++) {
+        Wire.beginTransmission(address);
+        uint8_t error = Wire.endTransmission();
+
+        if (error == 0) {
+            AddLogInfo("I2CBus", "Found device at 0x%02X", address);
+            count++;
+        }
+    }
+
+    if (count == 0) {
+        AddLogWarn("I2CBus", "No I2C devices found.");
+    } else {
+        AddLogInfo("I2CBus", "Scan complete. %d device(s) found.", count);
+    }
+}
--- src/hal/I2CBus.h
+++ src/hal/I2CBus.h
@@ -7,6 +7,7 @@ public:
     bool Init();
     bool Write(uint8_t address, uint8_t reg, uint8_t data);
     bool Write(uint8_t address, const uint8_t* data, uint8_t length);
+    void Scan();
 
 private:
     uint8_t _sda;
